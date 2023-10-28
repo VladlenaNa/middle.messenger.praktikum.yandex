9f45706ef4feab6ad099364619a6ee274ec1d063
Vlada <vladaa1998@yandex.ru>
2023-10-28 23:41:40 +0500

    first sprint

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { partials } from 'handlebars';
+import { resolve } from 'path/posix';
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
+import card from "./src/layouts/card/card";
+
+export default defineConfig({
+    root: resolve(__dirname),
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src/partials'),
+            helpers: {
+                card,
+            },
+        }) as unknown as Plugin,
+    ],
+
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'src/pages/login/login.html'),
+            }
+        }
+    },
+    assetsInclude: "**/*.hbs"
+})
