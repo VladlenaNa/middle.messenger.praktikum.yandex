aab88c9818be8a9260f75077fcb1765e7097f239
Vlada <vladaa1998@yandex.ru>
2023-10-29 03:20:56 +0500

    fix server.ts

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 
 import express from 'express';
+import { resolve } from 'path';
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(resolve(__dirname, 'src')));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
